cc53b28da1de83ca7261505e1a69d228eab9db90
Matt Oquist <moquist@majen.net>
2015-01-28 14:31:51 -0500

    Preserve gross movie cast listing query app.
    TODO: refactor this puppy.

--- src/rottentomatoes_query/core.cljs
+++ src/rottentomatoes_query/core.cljs
@@ -3,7 +3,28 @@
 
 (nodejs/enable-util-print!)
 
-(defn -main []
-  (println "Hello world!"))
+(def request (nodejs/require "request"))
+
+(def api-key (-> (.-env nodejs/process) js->clj
+                 ;; Feel free to substitute your own API key in your
+                 ;; environment, and just change the name here.
+                 (get "RT_API_KEY")))
+
+(def api-base "http://api.rottentomatoes.com/api/public/v1.0")
+
+(defn -main [& args]
+  (let [rq (str api-base "/movies.json" "?q=" (first args) "&apikey=" api-key)]
+    (.get request rq
+          (fn [err resp]
+            (let [d (-> resp .-body js/JSON.parse js->clj)
+                  movies (get d "movies")
+                  titles (map #(get % "title") movies)
+                  cast-url (get-in (first movies) ["links" "cast"])
+                  cast-url (str cast-url "?apikey=" api-key)]
+              (.get request cast-url
+                    (fn [err resp]
+                      (let [data (-> resp .-body js/JSON.parse js->clj)
+                            cast (get data "cast")]
+                        (dorun (map #(prn (get % "name")) cast))))))))))
 
 (set! *main-cli-fn* -main)

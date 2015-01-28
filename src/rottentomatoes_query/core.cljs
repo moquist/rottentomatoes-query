47ab200249ce6015624a963b012c892cebb131e6
Matt Oquist <moquist@majen.net>
2015-01-28 14:39:13 -0500

    Refactor in prep for meetup collaboration.
    * query for movies with titles matching "The Sting"
    * print list of matching titles & URL for querying the cast of each

--- src/rottentomatoes_query/core.cljs
+++ src/rottentomatoes_query/core.cljs
@@ -12,19 +12,15 @@
 
 (def api-base "http://api.rottentomatoes.com/api/public/v1.0")
 
+(def title "The Sting")
+
 (defn -main [& args]
-  (let [rq (str api-base "/movies.json" "?q=" (first args) "&apikey=" api-key)]
+  (let [rq (str api-base "/movies.json" "?q=" title "&apikey=" api-key)]
     (.get request rq
           (fn [err resp]
             (let [d (-> resp .-body js/JSON.parse js->clj)
-                  movies (get d "movies")
-                  titles (map #(get % "title") movies)
-                  cast-url (get-in (first movies) ["links" "cast"])
-                  cast-url (str cast-url "?apikey=" api-key)]
-              (.get request cast-url
-                    (fn [err resp]
-                      (let [data (-> resp .-body js/JSON.parse js->clj)
-                            cast (get data "cast")]
-                        (dorun (map #(prn (get % "name")) cast))))))))))
+                  movies (get d "movies")]
+              (dorun (map #(println :title (get % "title") "\n\t"
+                                    :cast-query-url (get-in % ["links" "cast"])) movies)))))))
 
 (set! *main-cli-fn* -main)
